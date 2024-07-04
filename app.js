f4afade3b201e6c580eba9bd0b3e5350d3b898d6
Bunma93 <bunma93@gmail.com>
2024-07-04 12:35:11 +0700

    Js-obj12

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // write code here
+const employees = {
+    john: { salary: 1000, address: { district: 'Ratchathewi', province: 'Bangkok' } },
+    peter: { salary: 1500, address: { district: 'Pathumwan', province: 'Bangkok' } },
+    mike: { salary: 800, address: { district: 'Pakkret', province: 'Nonthaburi' } },
+    sarah: { salary: 2200, address: { district: 'Sriraja', province: 'Chonburi' } }
+  };
+
+function checkEmployeeInfo(name) {
+    if (employees[name]) {
+        alert(`Name: ${name}, Salary: ${employees[name].salary}, Address: ${employees[name].address.district}, ${employees[name].address.province}`)
+    } else {
+        alert("Not Found");
+    }
+}
+
+let name = prompt("กรอกชื่อพนักงาน");
+checkEmployeeInfo(name);
+
